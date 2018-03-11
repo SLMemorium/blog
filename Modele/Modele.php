52a007ab932f198fcfd211971d888bc5360467f1
SLMemorium <fsommay@gmail.com>
2018-03-11 20:12:34 +0100

    Update Modele.php

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -6,7 +6,7 @@ abstract class Modele
 
     public function __construct()
     {
-        $db = new PDO('mysql:host=db710898991.db.1and1.com;dbname=db710898991;charset=utf8', 'dbo710898991', 'Soundlike21@');
+        $db = new PDO('mysql:host=db710898991.db.1and1.com;dbname=db710898991;charset=utf8', 'dbo710898991', '*********');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
         $this->_db = $db;
     }
